9810c079d31932daf52bd35c9fe9bea7008df400
Admin <criphood4life@gmail.com>
2023-03-19 22:07:42 +0300

    feat: add redirects

--- src/widgets/router/AppRouter.tsx
+++ src/widgets/router/AppRouter.tsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, Navigate } from 'react-router-dom';
 import Main from '../../pages/Main/Main';
 import About from '../../pages/About/About';
 import Error from '../../pages/Error/Error';
@@ -8,9 +8,10 @@ class AppRouter extends Component {
   render() {
     return (
       <Routes>
-        <Route path={'/'} element={<Main />}></Route>
-        <Route path={'/about'} element={<About />}></Route>
-        <Route path={'*'} element={<Error />}></Route>
+        <Route path={'/'} element={<Main />} />
+        <Route path={'/about'} element={<About />} />
+        <Route path={'*'} element={<Navigate to="/404" replace />} />
+        <Route path="/404" element={<Error />} />
       </Routes>
     );
   }
---
+++ src/widgets/search/utils/utils.js
@@ -0,0 +1,11 @@
+function setState(value) {
+    localStorage.setItem('cripInput', JSON.stringify(value));
+}
+function getState() {
+    const item = localStorage.getItem('cripInput');
+    if (typeof item === 'string') {
+        return JSON.parse(item);
+    }
+    return '';
+}
+export { setState, getState };
