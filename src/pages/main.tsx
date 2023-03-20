5cf48a3c9aa559c4579fef45b0fd1f34b33546d7
Admin <criphood4life@gmail.com>
2023-03-20 23:01:11 +0300

    fix: delete duplicate names

--- src/pages/main.tsx
+++ src/pages/main.tsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
-import Products from '../widgets/products/products';
-import Search from '../widgets/search/search';
+import Products from '../widgets/cards/products';
+import Search from '../widgets/search-input/search';
 
 class Main extends Component {
   render() {
